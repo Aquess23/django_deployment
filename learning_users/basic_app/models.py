d72ce0f0440f93235097d0ac2ef8d25024d58bc0
Agnieszka Chmielnicka <agnieszka.chmielnicka@ericsson.com>
2018-12-04 22:44:04 +0100

    models.py - adding on_delete to User model

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
 
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     portfoilo_site = models.URLField(blank=True)
 
